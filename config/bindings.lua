4a2768a4f4febd5c98aed3a916ea563475f8978b
gheroufosse <gheroufosse>
2026-09-09 12:17:52 +0200

    Update tab switching to Ctrl+Tab for Belgian AZERTY keyboard compatibility

--- config/bindings.lua
+++ config/bindings.lua
@@ -68,8 +68,8 @@ local keys = {
    { key = 'w',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
 
    -- tabs: navigation
-   { key = '[',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
-   { key = ']',          mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
+   { key = 'Tab',        mods = 'CTRL',        action = act.ActivateTabRelative(1) },
+   { key = 'Tab',        mods = 'CTRL|SHIFT',  action = act.ActivateTabRelative(-1) },
    { key = '[',          mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
    { key = ']',          mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
 
